f6ca73ba1fbdf6096c2966d02da6b98fc79e1f90
Logi13 <johnny.lioup@gmail.com>
2018-12-09 20:38:41 +0200

    Added comments in the Sheet project, started Chapter 15 and created the first example

--- Testing/Sheet/mainwindow.cpp
+++ Testing/Sheet/mainwindow.cpp
@@ -6,10 +6,10 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-
+    // Test of image label
     QPixmap pix("C:/Users/johnn/Documents/Qt-Tutorials/Testing/Sheet/Picture.png");
     ui->label_pic->setPixmap(pix);
-
+    // Changes the label depending the connection to the db
     if(!connOpen())
         ui->label->setText("Failed to open the database");
     else
@@ -24,20 +24,22 @@ MainWindow::~MainWindow()
 void MainWindow::on_loginButton_clicked()
 {
     QString username, password;
-
+    // Get the data from the lineEdits(username,password)
     username = ui->lineEdit_username->text();
     password = ui->lineEdit_password->text();
-
+    // qDebug messsage
     if(!connOpen()) {
         qDebug() << "Failed to open the database";
         return;
     }
-
+    // Open the connection
     connOpen();
     QSqlQuery qry;
+    // prepares the query message
     qry.prepare("select * from fighter where username='"+username +"' and password='"+password +"'");
 
     if(qry.exec()) {
+        // A simple check if the username and password exists and if it duplicate
         int count = 0;
         while(qry.next())
         {
@@ -58,20 +60,25 @@ void MainWindow::on_loginButton_clicked()
 
 void MainWindow::on_pushButton_tableLoad_clicked()
 {
+    // Table load if login was successful
     QSqlQueryModel *model = new QSqlQueryModel();
     connOpen();
     QSqlQuery *qry = new QSqlQuery(mydb);
-
+    //Prepares the query message
     qry->prepare("select Level,Skills,Description from fighter");
-
+    //executes the query
     qry->exec();
+    //Insert the table in the model
     model->setQuery(*qry);
+    // Display the table
     ui->tableView->setModel(model);
+    //row counts qDebug message for testing purposes
     qDebug() << (model->rowCount());
 
     connClose();
 }
 
+// Database access based on combobox without password or username
 void MainWindow::on_comboBox_currentIndexChanged(const QString &arg1)
 {
     QString cl = ui->comboBox->currentText();
